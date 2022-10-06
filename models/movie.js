36b3ed14aecce5c83ad5e519da2d01b6f42f47cc
HeilRulez <Rulez.Heil@gmail.com>
2022-10-07 01:47:47 +0600

    готовы схемы

---
+++ models/movie.js
@@ -0,0 +1,74 @@
+const mongoose = require('mongoose');
+const { regExp } = require('../constants/constants');
+
+const ObjectID = mongoose.Schema.Types.ObjectId;
+
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: [true, 'Не должно быть пустым'],
+  },
+  director: {
+    type: String,
+    required: [true, 'Не должно быть пустым'],
+  },
+  duration: {
+    type: Number,
+    required: [true, 'Не должно быть пустым'],
+  },
+  year: {
+    type: String,
+    required: [true, 'Не должно быть пустым'],
+  },
+  description: {
+    type: String,
+    required: [true, 'Не должно быть пустым'],
+  },
+  image: {
+    type: String,
+    required: [true, 'Не должно быть пустым'],
+    validate: {
+      validator(v) {
+        return regExp.link.test(v);
+      },
+    },
+  },
+  trailerLink: {
+    type: String,
+    required: [true, 'Не должно быть пустым'],
+    validate: {
+      validator(v) {
+        return regExp.link.test(v);
+      },
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: [true, 'Не должно быть пустым'],
+    validate: {
+      validator(v) {
+        return regExp.link.test(v);
+      },
+    },
+  },
+  owner: {
+    type: ObjectID,
+    ref: 'user',
+    required: [true, 'Не должно быть пустым'],
+  },
+  movieID: {
+    type: ObjectID,
+    ref: 'movie',
+    required: [true, 'Не должно быть пустым'],
+  },
+  nameRU: {
+    type: String,
+    required: [true, 'Не должно быть пустым'],
+  },
+  nameEN: {
+    type: String,
+    required: [true, 'Не должно быть пустым'],
+  },
+});
+
+module.exports = mongoose.model('movie', movieSchema);
---
+++ models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Не должно быть постым'],
+    minlength: [2, 'Должно быть не менее {VALUE}'],
+    maxlength: [30, 'Должно быть не более {VALUE}'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Не должно быть пустым'],
+    unique: true,
+  },
+  password: {
+    type: String,
+    select: false,
+    required: [true, 'Не должно быть постым'],
+  },
+});
+
+module.exports = mongoose.model('user', userSchema);
+
+userSchema.methods.toJSON = function () {
+  const user = this.toObject();
+  delete user.password;
+  return user;
+};
