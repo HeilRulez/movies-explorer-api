22dc50e07757260cade45747a8a185824061898a
HeilRulez <rulez.heil@gmail.com>
2022-10-13 01:00:20 +0600

    ревью

--- constants/constants.js
+++ constants/constants.js
@@ -1,5 +1,5 @@
 module.exports = {
-  corsUrl: 'http://localhost:3001',
+  corsUrl: 'http://tvoyomesto.nomorepartiesxyz.ru',
   regExp: {
     link: /^https?:\/\/(?:www.)?[-a-zA-Z0-9:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/,
   },
